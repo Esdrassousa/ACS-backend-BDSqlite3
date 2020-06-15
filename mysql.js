b7ec4364a87950d3570ac7546413242021c7e96d
Esdras de Sousa <esdrassousasoares@hotmail.com>
2020-06-15 18:39:30 -0300

    mudança sql

--- mysql.js
+++ mysql.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2')
 
-var pool =  mysql.createPool({
+/* var pool =  mysql.createPool({
     "user" : process.env.MYSQL_USER,
     "password" : process.env.MYSQL_PASSWORD,
     "database" : process.env.MYSQL_DATABASE,
@@ -10,10 +10,26 @@ var pool =  mysql.createPool({
 
 })
 
-exports.pool = pool;
+exports.pool = pool; */
 
 /* 
 pool.connect(function(err){
   if(err) return console.log(err);
   console.log('conectou!');
 }) */
+
+
+var connection =  mysql.createConnection({
+    "host" : 'us-cdbr-east-05.cleardb.net',
+    "user" : 'b9d29dbf4876c1',
+    "database" : 'heroku_6ac9ab071661142',
+    "password" : 'c127899f',
+    
+})
+
+connection.connect(function(err){
+    if(err) return console.log(err);
+    console.log('conectou!');
+  })
+    
+    //"port" : process.env.MYSQL_PORT
