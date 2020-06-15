f0e9ff12adcc12f739d66316843f3a91e6c9f89f
Esdras de Sousa <esdrassousasoares@hotmail.com>
2020-06-15 01:19:57 -0300

    connection

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -8,14 +8,32 @@ const authorization = require('../services/auth-service')
 exports.post = async (req, res) => {
 
     try {
-        await connection('ACS').insert({
+        /* await connection('acs').insert({
             id: crypto.randomBytes(4).toString('HEX'),
             nome: req.body.nome,
             email: req.body.email,
             senha: md5(req.body.senha + global.SALT_KEY)
 
+        }) */
+
+        await connection.getConnection((error , conn)=>{
+            conn.query(
+                'INSERT INTO acs (id, nome, email, senha) VALUES (?,?,?,?)',
+                [ crypto.randomBytes(4).toString('HEX'),
+                req.body.nome,
+                req.body.email,
+                md5(req.body.senha + global.SALT_KEY)],
+                (error , resultado , field)=>{
+                    conn.release();
+
+                    if(error ){
+                       return res.status(201).send({error :error , response:null})
+                    }
+                    res.status(201).send({ message: 'cadastrado com sucesso' })
+                }
+            )
         })
-        res.status(201).send({ message: 'cadastrado com sucesso' })
+        //res.status(201).send({ message: 'cadastrado com sucesso' })
 
     } catch (e) {
         console.log(e);
@@ -25,13 +43,13 @@ exports.post = async (req, res) => {
 
 exports.get = async (req, res) => {
 
-    const users = await connection('ACS').select('*')
+    const users = await connection('acs').select('*')
     res.status(201).send(users)
 }
 
 exports.getById = async (req, res) => {
 
-    const users = await connection('ACS').where().select('*')
+    const users = await connection('acs').where().select('*')
     res.status(201).send(users)
 }
 
@@ -43,7 +61,7 @@ exports.authentication = async (request, response ,next) => {
         const senha = md5(request.body.senha + global.SALT_KEY)
 
 
-        const user = await connection('ACS').where({
+        const user = await connection('acs').where({
             email: email, 
             senha: senha,
         }).first()//.select('id')
@@ -77,7 +95,7 @@ exports.delete = async(req, res) =>{
 
     try{
 
-    const user = await connection('ACS').where('id' , id).delete();
+    const user = await connection('acs').where('id' , id).delete();
 
     if (!user){
         res.status(401).send({message:'nao foi possivel deletar'})
